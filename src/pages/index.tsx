1a475a2b7c215f154959898f98aa50df8171a3cb
MinaMalak-cmd <engmina15@yahoo.com>
2022-03-19 15:53:24 +0200

    add material UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Counter } from "../Components/Counter";
+import { Example2 } from "../Examples/Example2/Example2";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello React.js Testing Series Friends!!!</h1>
-      <Counter description="My Counter" defaultCount={9} />
+      {/* <Counter description="My Counter" defaultCount={9} /> */}
+      <Example2 onMoney={(e: number) => console.log(e)} />
     </div>
   );
 }
